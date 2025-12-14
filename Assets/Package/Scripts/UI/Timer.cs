645ee940155e64359cc777bc4f436e0de11dfbb3
EnzoCampa <enzo.campanella@outlook.fr>
2025-12-14 14:13:35 +0100

    son du timer

--- Assets/Package/Scripts/UI/Timer.cs
+++ Assets/Package/Scripts/UI/Timer.cs
@@ -4,18 +4,31 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     [SerializeField] public float time;
+
+    [SerializeField] private AudioSource timerSound;
+    [SerializeField] private AudioClip timerSoundClip;
+
     public Text TimerText;
     public Image Fill;
     public float Max;
+    public bool Btimer = true;
+
 
     private bool hasTriggeredDefeat = false; // Évite les appels multiples
-
+    private void Start()
+    {
+        timerSound.clip = timerSoundClip;
+    }
     void Update()
     {
         time -= Time.deltaTime;
         TimerText.text = "" + (int)time;
         Fill.fillAmount = time / Max;
-
+        if (time <= 10 && Btimer)
+        {
+            timerSound.Play();
+            Btimer = false;
+        }
         if (time <= 0 && !hasTriggeredDefeat)
         {
             hasTriggeredDefeat = true;
